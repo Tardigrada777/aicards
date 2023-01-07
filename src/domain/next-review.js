dd60dee60d55a59183f844f416a4358e69936ef4
Vladimir Myltcev <ntwrite@gmail.com>
2023-01-07 10:12:49 +0100

    fix typo

--- src/domain/next-review.js
+++ src/domain/next-review.js
@@ -6,28 +6,28 @@ const intervalsMap = () => {
     hard: 1,
     normal: 6,
     good: 8,
-    ease: 12,
+    easy: 12,
   });
 
   intervalsMap.set(12, {
     hard: 14,
     normal: 18,
     good: 20,
-    ease: 24,
+    easy: 24,
   });
 
   intervalsMap.set(24, {
     hard: 14,
     normal: 18,
     good: 20,
-    ease: 24,
+    easy: 24,
   });
 
   intervalsMap.set(36, {
     hard: 16,
     normal: 22,
     good: 24,
-    ease: 32,
+    easy: 32,
   });
 
   return map;
@@ -43,7 +43,7 @@ const currentInterval = (interval) => {
 /**
  * 
  * @param {number} interval    Int (hours)
- * @param {'hard' | 'normal' | 'good' | 'ease'} complexity  Complexity to remember card.
+ * @param {'hard' | 'normal' | 'good' | 'easy'} complexity  Complexity to remember card.
  * @returns {number} hours that should be added.
  */
 export const nextReview = (interval, complexity) => {
