723453676936cacf88e75390842bffee77be5db9
Vladimir Myltcev <ntwrite@gmail.com>
2023-01-07 12:33:56 +0100

    build

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 export const client = new ApolloClient({
   uri: 'https://content-hyena-32.hasura.app/v1/graphql',
   headers: {
-    'x-hasura-admin-secret': import.meta.env.HASURA_SECRET,
+    'x-hasura-admin-secret': import.meta.env.VITE_HASURA_SECRET,
   },
   cache: new InMemoryCache(),
 });
