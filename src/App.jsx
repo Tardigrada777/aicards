e3d638d88b5415e56000648aeb1c8f7346b7f986
Gedeon4ick <pavell720@gmaul.com>
2023-01-07 14:43:42 +0600

    componentsLogin

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useQuery } from '@apollo/client';
 import Cards from './Cards';
+import Answer from './components/answer/Answer';
+import Login from './components/login/Login';
 
 function App() {
   return (
     <div className="app">
       <Cards />
+      <Login/>
+      <Answer/>
     </div>
   )
 }
---
+++ src/components/answer/Answer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './answer.scss'
+
+const Answer = () => {
+    return (
+        <div className='container'>
+            <div className="wrapp">
+                <div className="title">
+                    You're good, there's no training for today
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Answer;
