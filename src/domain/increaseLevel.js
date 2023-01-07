2ab733c9b399583f555381a2d6bca2a5137a7fbf
Elijah <62115378+elijahmylcev@users.noreply.github.com>
2023-01-07 15:40:03 +0600

    function IncreaseLevel (#2)
    * function IncreaseLevel
    
    * function IncreaseLevel
    
    * delete dir__functions
    
    Co-authored-by: Vladimir Myltcev <ntwrite@gmail.com>

---
+++ src/domain/increaseLevel.js
@@ -0,0 +1,9 @@
+export default function increaseLevel(level, answer) {
+  const levels = {
+    easy: (level) => level + 25,
+    medium: (level) => level + 15,
+    good: (level) => level + 10,
+    hard: (level) => level -10,
+  };
+  return answer in levels ? levels[answer](level) > 100 ? 100: levels[answer](level) : level;
+}
